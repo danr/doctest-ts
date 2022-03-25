fda5017a6625916732cc50fa8a9761a971e29dc8
pietervdvn <pietervdvn@posteo.net>
2022-03-25 16:36:18 +0100

    Small tweaks

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ function readDirRecSync(path: string, maxDepth = 999): string[] {
         if (stats.isDirectory()) {
             // Subdirectory
             // @ts-ignore
-            result.push(...ScriptUtils.readDirRecSync(fullEntry, maxDepth - 1))
+            result.push(...readDirRecSync(fullEntry, maxDepth - 1))
         } else {
             result.push(fullEntry)
         }
@@ -22,31 +22,36 @@ function readDirRecSync(path: string, maxDepth = 999): string[] {
     return result;
 }
 
-function main(){
-    
-const args = process.argv
-console.log(args.join(","))
-const directory = args[2].replace(/\/$/, "")
-if(directory === "--require"){
-    console.error("Probably running the testsuite, detects '--require' as second argument. Quitting now")
-    return;
-}
-if(directory === undefined){
-    console.log("Usage: doctest-ts-improved <directory under test>. This will automatically scan recursively for '.ts'-files, excluding 'node_modules' '*.doctest.ts'-files")
-}
+function main() {
 
-const files = readDirRecSync(directory).filter(p => !p.startsWith("./node_modules") && !p.endsWith(".doctest.ts"))
-const noTests : string[] = []
-for (const file of files) {
-    const generated = new TestCreator(file).createTest()
-    if(generated === 0){
-        noTests.push(file)
-    }else{
-        console.log("Generated tests for "+file+" ("+generated+" tests found)")
+    const args = process.argv
+    console.log(args.join(","))
+    const directory = args[2].replace(/\/$/, "")
+    if (directory === "--require") {
+        console.error("Probably running the testsuite, detects '--require' as second argument. Quitting now")
+        return;
+    }
+    if (directory === undefined) {
+        console.log("Usage: doctest-ts-improved <directory under test>. This will automatically scan recursively for '.ts'-files, excluding 'node_modules' '*.doctest.ts'-files")
+    }
+
+    const files = readDirRecSync(directory)
+        .filter(p => p.endsWith(".ts"))
+        .filter(p => p.indexOf("/node_modules/") < 0 && !p.endsWith(".doctest.ts"))
+    const noTests: string[] = []
+    for (let i = 0; i < files.length; i++){
+        const file = files[i];
+        process.stdout.write(`\r (${i}/${files.length}) inspecting ${file}                                     \r`)
+        const generated = new TestCreator(file).createTest()
+        if (generated === 0) {
+            noTests.push(file)
+        } else {
+            console.log("Generated tests for " + file + " (" + generated + " tests found)")
+        }
+    }
+    if (noTests.length > 0) {
+        console.log("No tests found in: " + noTests.join(", "))
     }
 }
-if(noTests.length > 0){
-    console.log("No tests found in: "+noTests.join(", "))
-}
-}
+
 main()
